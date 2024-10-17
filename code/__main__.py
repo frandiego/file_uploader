110c8316a8062778a3d15fad8bec7cf042e3ee59
frandiego <franciscojosediegoacosta@gmail.com>
2024-10-17 18:05:02 +0200

    :zap: clean code

--- code/__main__.py
+++ code/__main__.py
@@ -3,7 +3,7 @@ from src import SRC
 src = SRC.cli()
 src.structure.make()
 src.pcloud.upload(
-    path=src.structure.path, 
-    extensions=src.structure.extensions +  src.structure.raw_extensions, 
-    cores=src.structure.cores, 
+    path=src.structure.path,
+    extensions=src.structure.extensions + src.structure.raw_extensions,
+    cores=src.structure.cores,
 )
